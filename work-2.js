63bb91b2ba0b565e2004c3a05b0a8f0f2c18ea53
Hovo Mnatsakanyan <51056620+Hovo91@users.noreply.github.com>
2019-09-02 14:51:54 +0400

    Create work-2.js

---
+++ work-2.js
@@ -0,0 +1,32 @@
+// Write a JavaScript function to get all possible subsets of length 3 of the given array.
+Assume that all elements in the array are unique.
+
+function getAllPossibleSubsetsOfLengthThree (array = []) {
+  if (array.length < 3) {
+    return array;
+  }
+  let result = [];
+  let mainArray = [];
+  for (let i = 0; i <= 3; i++) {
+      for (let j = i + 1; j <= i + 4; j++) {
+        if (checkingNotUndefined(array, j)) {
+          for (let z = j + 1; z <= j + 4; z++) {
+            if (checkingNotUndefined(array, z)) {
+              result.push(array[i]);
+              result.push(array[j]);
+              result.push(array[z]);
+              mainArray.push(result);
+              result = [];
+            }
+          }
+        }
+      }
+    }
+  return mainArray;
+}
+
+function checkingNotUndefined(array, i) {
+  return array[i] === undefined ? false : true;
+}
+
+console.log(getAllPossibleSubsetsOfLengthThree([5, 9, 23, 0, -2, -1]));
