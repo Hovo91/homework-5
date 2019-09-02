53071c58c4c9c9617307fa31d353e79221ec3f2f
Hovo Mnatsakanyan <51056620+Hovo91@users.noreply.github.com>
2019-09-02 18:55:35 +0400

    Update work-2.js

--- work-2.js
+++ work-2.js
@@ -7,26 +7,18 @@ function getAllPossibleSubsetsOfLengthThree (array = []) {
   }
   let result = [];
   let mainArray = [];
-  for (let i = 0; i <= 3; i++) {
-      for (let j = i + 1; j <= i + 4; j++) {
-        if (checkingNotUndefined(array, j)) {
-          for (let z = j + 1; z <= j + 4; z++) {
-            if (checkingNotUndefined(array, z)) {
-              result.push(array[i]);
-              result.push(array[j]);
-              result.push(array[z]);
-              mainArray.push(result);
-              result = [];
-            }
-          }
-        }
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      for (let z = j + 1; z < array.length; z++) {
+        result.push(array[i]);
+        result.push(array[j]);
+        result.push(array[z]);
+        mainArray.push(result);
+        result = [];
       }
     }
+  }
   return mainArray;
-}
-
-function checkingNotUndefined(array, i) {
-  return array[i] === undefined ? false : true;
 }
 
 console.log(getAllPossibleSubsetsOfLengthThree([5, 9, 23, 0, -2, -1]));
